e298278114872efd13ffd530642700b38f4a7eb0
Krishna-coder6111 <70067302+Krishna-coder6111@users.noreply.github.com>
2024-04-25 00:24:52 -0400

    cors error

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,43 +1,37 @@
-import * as dao from "./dao.js";
-
-export default function ModuleRoutes(app) {
-  app.get("/api/modules", async (req, res) => {
-    const modules = await dao.findAllModules();
-    res.json(modules);
-  });
-
-  app.get("/api/modules/:moduleId", async (req, res) => {
-    const { moduleId } = req.params;
-    const module = await dao.findModuleById(moduleId);
-    if (!module) {
-      res.status(404).send("Module not found");
-      return;
-    }
-    res.json(module);
-  });
-
-  app.put("/api/modules/:moduleId", async (req, res) => {
-    const { moduleId } = req.params;
-    const status = await dao.updateModule(moduleId, req.body);
-    res.json(status);
-  });
-
-  app.delete("/api/modules/:moduleId", async (req, res) => {
-    const { moduleId } = req.params;
-    const status = await dao.deleteModule(moduleId);
-    res.json(status);
-  });
-
-  app.post("/api/courses/:courseId/modules", async (req, res) => {
-    const { courseId } = req.params;
-    const newModule = { ...req.body, courseId };
-    const module = await dao.createModule(newModule);
-    res.json(module);
-  });
-
-  app.get("/api/courses/:courseId/modules", async (req, res) => {
-    const { courseId } = req.params;
-    const modules = await dao.findModulesByCourseId(courseId);
-    res.json(modules);
+import Database from "../Database/index.js";
+export default function CourseRoutes(app) {
+    app.get("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        const course = Database.courses
+          .find((c) => c._id === id);
+        if (!course) {
+          res.status(404).send("Course not found");
+          return;
+        }
+        res.send(course);
+      });
+    app.put("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        const course = req.body;
+        Database.courses = Database.courses.map((c) =>
+          c._id === id ? { ...c, ...course } : c
+        );
+        res.sendStatus(204);
+      });    
+    app.delete("/api/courses/:id", (req, res) => {
+        const { id } = req.params;
+        Database.courses = Database.courses
+          .filter((c) => c._id !== id);
+        res.sendStatus(204);
+      });    
+    app.post("/api/courses", (req, res) => {
+        const course = { ...req.body,
+          _id: new Date().getTime().toString() };
+        Database.courses.push(course);
+        res.send(course);
+      });    
+  app.get("/api/courses", (req, res) => {
+    const courses = Database.courses;
+    res.send(courses);
   });
 }
